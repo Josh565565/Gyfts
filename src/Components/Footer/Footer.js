97a8b1beb90528ee910b209deeed29a43006e41b
Okwor Joshua <smartcash565@gamil.com>
2023-09-25 05:16:23 +0100

    update

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./FooterStyle.module.css";
+
+function Footer() {
+  return <div className={styles.con}>Footer</div>;
+}
+
+export default Footer;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./HeaderStyle.module.css";
+
+function Header() {
+  return <div className={styles.con}>Header</div>;
+}
+
+export default Header;
