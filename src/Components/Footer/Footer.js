ad4133c2fc6084d7279ab5b1c60b1a64a072b894
Sunday chinaza <chinazasundaygoodness@gmai.com>
2023-09-26 12:52:38 +0100

    footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,44 +1,56 @@
 import React from 'react';
 import styles from "./FooterStyle.module.css"; // Import the CSS module
 
+import call from "../Assets/call.png";
+import Vector from "../Assets/Vector.svg";
+import facebook from "../Assets/facebook-icon.png";
+import instagram from "../Assets/instagram-icon.png";
+import twitter from "../Assets/twitter-icon.png";
+import linkedin from "../Assets/linkdln-icon.png";
+
 const Footer = () => {
   return (
     <footer className={styles["landing-page-footer"]}>
       <div className={styles["left-section"]}>
-        <div>
-          <p>Gyft.ng</p>
-          <p>We offer a wide selection of unique</p>
-          <p>And high quality gifts for everyone</p>
+        <div className={styles["gifts"]}>
+          <p className={styles["logo"]}>Gyft.ng</p>
+          <p>We offer a wide selection of unique
+          nd high-quality gifts for every occasion</p>
         </div>
-        <p>&copy;2023 Gifts.ng. All rights reserved</p>
+        <p>copyrights &copy;2023 Gifts.ng. All rights reserved</p>
       </div>
       <div className={styles["middle-section"]}>
         <div className={styles["support"]}>
           <p>Support</p>
           <p>
-            <i className="fas fa-phone"></i> +23470655432124
+            <img className={styles["call-icon"]} src={call} alt="Call Icon" /> +23470655432124
           </p>
           <p>
-            <img src="email-icon.png" alt="Email Icon" /> officialgyfts24@gmail.com
+            <img className={styles["email-icon"]} src={Vector} alt="Vector Icon" /> officialgyfts24@gmail.com
           </p>
         </div>
+        <p>Social Links</p>
         <div className={styles["social-links"]}>
-          {/* Sample link to Facebook */}
+          
           <a href="https://www.facebook.com/sample" target="_blank" rel="noopener noreferrer">
-            <i className="fab fa-facebook"></i>
+            <img className={styles["social-icon"]} src={facebook} alt="Facebook Icon" />
           </a>
-          <i className="fab fa-instagram"></i>
-          <i className="fab fa-twitter"></i>
-          <i className="fab fa-linkedin"></i>
-          {/* Include an image for the social media section */}
-          <img src="social-icon.png" alt="Social Icon" />
-        </div>
+          <a href="https://www.instagram.com/sample" target="_blank" rel="noopener noreferrer">
+          <img className={styles["social-icon"]} src={instagram} alt="Instagram Icon" />
+          </a>
+          <a href="https://www.twitter.com/sample" target="_blank" rel="noopener noreferrer">
+          <img className={styles["social-icon"]} src={twitter} alt="Twitter Icon" />
+            </a>
+            <a href="https://www.linkedin.com/sample" target="_blank" rel="noopener noreferrer">
+          <img className={styles["social-icon"]} src={linkedin} alt="LinkedIn Icon" />
+          </a>
+          </div>
       </div>
       <div className={styles["right-section"]}>
         <p>Subscribe to our Newsletter</p>
-        <p>For Latest News and updates</p>
+        <p className={styles["latest"]}>For Latest News and updates</p>
         <form>
-          <input type="email" placeholder="Enter your email" />
+          <input type="email" placeholder="Email Address" /> <br></br>
           <button type="submit">Subscribe</button>
         </form>
       </div>
