e0b0a2c448eb87db5ddea0cee9cb807df04a8ef7
Sunday chinaza <chinazasundaygoodness@gmai.com>
2023-09-27 18:35:43 +0100

    Home page

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -11,6 +11,10 @@ import policy from '../Assets/policy.svg'; // Import the gift icon image
 import provision from '../Assets/provision.svg';
 import skincare from '../Assets/skincare.svg';
 import bucketwine from '../Assets/bucketwine.svg';
+import roseImage from '../Assets/roseImage.svg';
+import flowerImage from '../Assets/flowerImage.svg';
+import snacks from '../Assets/snacks.svg';
+import congratulationGift from'../Assets/congratulationGift.svg';
 
 function LandingPage() {
   return (
@@ -18,10 +22,12 @@ function LandingPage() {
       <div className={styles['content-box']}>
         <h1>Special Birthday Offer</h1>
         <p>Shop now and have 10% off your first order. Use the code BIRTHDAY10</p>
-        <div className={styles['bottom-container']}>
+
+        <button className={styles['shop-now-button']}>
           <img src={cart} alt="Cart Icon" className={styles['cart-icon']} />
-          <button>Shop now</button>
-        </div>
+          Shop Now
+        </button>
+        
       </div>
       <div className={styles['birthday']}>
             <img src={birthday} alt="birthday" />
@@ -51,13 +57,79 @@ function LandingPage() {
           </div>
         </form>
       </div>
-      <div className={styles['product-container']}>
+      <div className={styles['product-container-one']}>
+        <p>Trending Categories</p>
+        <p className={styles['product-name']}>Wedding Gift </p>
+        <div className={styles['product-box']}>
+        <div className={styles['bucketwine']}>
+            <img src={bucketwine} alt="wine" />
+          </div>
+       </div>
+
+        <div className={styles['product-box']}>
+        <div className={styles['provision']}>
+        <p className={styles['product-name']}>Food Basket </p>
+            <img src={provision} alt="provision" />
+          </div>
+          
+    </div>
+        <div className={styles['product-box']}>
+        <div className={styles['skincare']}>
+        <p className={styles['product-name']}>Gift for Women </p>
+            <img src={skincare} alt="skincare" />
+          </div>
+          
+        </div>
+      </div>
+
+      <div className={styles['product-container-two']}>
+        <p>Popular Categories</p>
+        <p className={styles['product-name']}>Birthday Gift </p>
+        <div className={styles['Flower-Gift']}>
+        <div className={styles['flowerImage']}>
+            <img src={flowerImage} alt="flower" />
+          </div>
+       </div>
+
+        <div className={styles['product-box']}>
+        <div className={styles['snacks-Gift']}>
+        <p className={styles['product-name']}>Anniversary Gift </p>
+            <img src={snacks} alt="snacks" />
+          </div>
+          
+    </div>
+        <div className={styles['product-box']}>
+        <div className={styles['congratulation']}>
+        <p className={styles['product-name']}>Congratulation Gift </p>
+            <img src={congratulationGift} alt="congratulation" />
+          </div>
+          
+        </div>
+      </div>
+
+
+
+      <div className={styles['rose-container']}>
+      <img src={roseImage} alt="Rose" className={styles['rose-image']} />
+      <div className={styles['rose-content']}>
+        <h2>Shop Flowers With Us</h2>
+        <p>Flowers Gift Basket</p>
+        <p>Thanks for shopping with us. You are welcome!</p>
+        <button className={styles['shop-now-button']}>
+          <img src={cart} alt="Cart Icon" className={styles['cart-icon']} />
+          Shop Now
+        </button>
+      </div>
+      </div>
+
+      
+      <div className={styles['product-container-two']}>
         <p>Top Picks</p>
         
         <div className={styles['product-box']}>
         <div className={styles['bucketwine']}>
             <img src={bucketwine} alt="wine" />
-          </div>1
+          </div>
           <p className={styles['product-name']}>Special Wedding Hamper </p>
           <p className={styles['product-price']}>₦30,000</p>
           <button className={styles['add-to-cart-button']}>Add to Cart</button>
