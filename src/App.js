1270510566a2715d9d1d06979dfe9605792e682d
Sunday chinaza <chinazasundaygoodness@gmai.com>
2023-10-08 16:15:39 +0100

    gift

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
 import HomePage from "./Components/HomePage/HomePage";
+import GiftOccasionsDropdown from "./Components/GiftOccasionsDropdown/GiftOccasionsDropdown";
+
 
 function App() {
   return (
     <div className="App">
       {/* <Footer /> */}
-      <HomePage />
+      <GiftOccasionsDropdown />
     </div>
   );
 }
