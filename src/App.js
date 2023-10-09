3d7f5480d0fee42edd17f7c80d5b0b2857580772
Okwor Joshua <smartcash565@gamil.com>
2023-10-09 11:38:37 +0100

    Added new pages

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
 import HomePage from "./Components/HomePage/HomePage";
+import ProductListingPage from "./Components/ProductListingPage/ProductListingPage";
+import ShoppingCartPage from "./Components/ShoppingCartPage/ShoppingCartPage";
+import Checkout from "./Components/CheckoutPage/Checkout";
+import ProductDetails from "./Components/ProductDetails/ProductDetails";
 
 function App() {
   return (
     <div className="App">
       {/* <Footer /> */}
-      <HomePage />
+      {/* <HomePage /> */}
+      {/* <ProductListingPage /> */}
+      {/* <ShoppingCartPage /> */}
+      {/* <Checkout /> */}
+      <ProductDetails />
     </div>
   );
 }
